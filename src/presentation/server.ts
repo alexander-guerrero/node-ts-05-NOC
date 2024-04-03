8d72d85d02e19e4ca3170a51fa55493182f82fd6
Alexander Guerrero <alex.guelu@gmail.com>
2024-04-03 08:02:54 -0500

    111. CRON Tasks

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,22 @@
+import { CronJob } from 'cron';
+
 export class Server {
 
     public static start() {
 
         console.log('Server started...');
+
+        const job = new CronJob(
+            '*/3 * * * * *', // cronTime
+            function () {
+                const date = new Date();
+                console.log('You will see this message every 3 seconds', date);
+            }, // onTick
+            null, // onComplete
+            true, // start
+            'America/Los_Angeles' // timeZone
+        );
+        // job.start() is optional here because of the fourth parameter set to true.
 
     }
 
