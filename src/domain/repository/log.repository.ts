be8f929417defb1572db9b6b3ed144493af4a508
Alexander Guerrero <alex.guelu@gmail.com>
2024-04-11 23:57:45 -0500

    125. FileSystem - GetLogs

--- src/domain/repository/log.repository.ts
+++ src/domain/repository/log.repository.ts
@@ -3,6 +3,6 @@ import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
 export abstract class LogRepository {
 
     abstract saveLog( log: LogEntity ): Promise<void>;
-    abstract getLogs( severityLevel: LogSeverityLevel ): Promise<LogEntity>;
+    abstract getLogs( severityLevel: LogSeverityLevel ): Promise<LogEntity[]>;
 
 }
