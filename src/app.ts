3139c7193ac00c146e731490f47017b933490f4b
Alexander Guerrero <alex.guelu@gmail.com>
2024-04-14 21:00:48 -0500

    131. Configurar variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/plugins/env.plugin";
 import { Server } from "./presentation/server";
 
 (() => {
@@ -6,5 +7,5 @@ import { Server } from "./presentation/server";
 
 function main() {
     // Server.start();
-    console.log(process.env.MAILER_EMAIL);
+    // console.log( envs );
 }
