69eccff26ebace4031a6dca5124f1615519c94b4
Alexander Guerrero <alex.guelu@gmail.com>
2024-04-20 12:58:36 -0500

    142. Enviar archivos adjuntos

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -2,13 +2,16 @@ import nodemailer from "nodemailer";
 import { envs } from "../../config/plugins/env.plugin";
 
 interface SendMailOptions {
-    to: string;
+    to: string | string[];
     subject: string;
     htmlBody: string;
-    // todo: attachments
+    attachments?: Attachment[];
 }
 
-// todo: Attachment 
+interface Attachment {
+    filename: string;
+    path: string;
+}
 
 export class EmailService {
 
@@ -22,14 +25,15 @@ export class EmailService {
 
     async sendEmail(options: SendMailOptions): Promise<boolean> {
 
-        const { to, subject, htmlBody } = options;
+        const { to, subject, htmlBody, attachments = [] } = options;
 
         try {
 
             const sentInformacion = await this.transporter.sendMail({
                 to,
                 subject,
-                html: htmlBody
+                html: htmlBody,
+                attachments
             });
 
             console.log(sentInformacion);
@@ -39,6 +43,26 @@ export class EmailService {
 
             return false;
         }
+
+    }
+
+    async sendEmailWithFileSystemLogs( to: string | string[] ) {
+
+        const subject = 'Logs del servidor';
+        const htmlBody = `
+            <h3>Logs de sistema - NOC</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p>Ver logs adjuntos</p>
+        `;
+        const attachments: Attachment[] = [
+            { filename: 'logs-all.log', path: './logs/logs-all.log' },
+            { filename: 'logs-high.log', path: './logs/logs-high.log' },
+            { filename: 'logs-medium.log', path: './logs/logs-medium.log' },
+        ];
+
+        return this.sendEmail({
+            to, subject, htmlBody, attachments
+        });
 
     }
 
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -18,15 +18,13 @@ export class Server {
 
         // Mandar email
         const emailService = new EmailService();
-        emailService.sendEmail({
-            to: 'alex.guelu@outlook.com',
-            subject: 'Logs de sistema',
-            htmlBody: `
-                <h3>Logs de sistema - NOC</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <p>Ver logs adjuntos</p>
-            `
-        });
+        emailService.sendEmailWithFileSystemLogs(
+            [
+                'alex.guelu@gmail.com',
+                'alex.guelu@outlook.com',
+                'lguerrero@ripley.com.pe'
+            ]
+        );
 
         // CronService.createJob(
         //     '*/5 * * * * *', // cronTime
