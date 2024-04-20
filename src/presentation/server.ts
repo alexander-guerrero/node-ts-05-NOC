1e7d951d466ec33263e0cde04de05839b4984549
Alexander Guerrero <alex.guelu@gmail.com>
2024-04-20 12:02:33 -0500

    141. Nodemailer - Gmail

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -3,6 +3,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailService } from "./email/email.service";
 
 // Se trabaja con las implementaciones (LogRepositoryImpl, FileSystemDatasource) de las clases abstractas (LogRepository, LogDatasource) 
 const fileSystemLogRepository = new LogRepositoryImpl(
@@ -16,7 +17,16 @@ export class Server {
         console.log('Server started...');
 
         // Mandar email
-        // console.log({ envs });
+        const emailService = new EmailService();
+        emailService.sendEmail({
+            to: 'alex.guelu@outlook.com',
+            subject: 'Logs de sistema',
+            htmlBody: `
+                <h3>Logs de sistema - NOC</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p>Ver logs adjuntos</p>
+            `
+        });
 
         // CronService.createJob(
         //     '*/5 * * * * *', // cronTime
