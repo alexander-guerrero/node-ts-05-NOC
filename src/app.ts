ca453c8827038db30fbba6b2d7dbd8366f8ddc30
Alexander Guerrero <alex.guelu@gmail.com>
2024-05-21 07:02:24 -0500

    154. Crear y leer de Mongo

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from "./config/plugins/env.plugin";
-import { MongoDatabase } from "./data/mongo";
+import { LogModel, MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
 
 (() => {
@@ -11,6 +11,20 @@ async function main() {
         mongoUrl: envs.MONGO_URL,
         dbName: envs.MONGO_DB_NAME
     });
+
+    // Crear una colección ~ tabla, documento ~ registro 
+    // const newLog = await LogModel.create({
+    //     message: 'Test message from Node to MongoDB - Severity High',
+    //     origin: 'app.ts',
+    //     level: 'high'
+    // });
+    // await newLog.save();
+    // console.log(newLog);
+
+    // Leer documentos ~ registros de la colección ~ tabla 
+    const logs = await LogModel.find();
+    console.log(logs);
+    console.log(logs[3].message);
 
     // Server.start();
 }
