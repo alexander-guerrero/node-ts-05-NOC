830405541c42a1d1956e4149b7e0525921208de7
Alexander Guerrero <alex.guelu@gmail.com>
2024-05-21 07:57:59 -0500

    155. MongoLogDatasource

--- src/app.ts
+++ src/app.ts
@@ -12,19 +12,5 @@ async function main() {
         dbName: envs.MONGO_DB_NAME
     });
 
-    // Crear una colección ~ tabla, documento ~ registro 
-    // const newLog = await LogModel.create({
-    //     message: 'Test message from Node to MongoDB - Severity High',
-    //     origin: 'app.ts',
-    //     level: 'high'
-    // });
-    // await newLog.save();
-    // console.log(newLog);
-
-    // Leer documentos ~ registros de la colección ~ tabla 
-    const logs = await LogModel.find();
-    console.log(logs);
-    console.log(logs[3].message);
-
     // Server.start();
 }
